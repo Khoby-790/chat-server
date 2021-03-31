cd3896fff5f551e37f6c44b64f04f32944a7c357
Khoby-790 <ebaidoo79@gmail.com>
2021-03-31 12:08:53 +0000

    --- Auto-Git Commit ---

--- tests/users.test.js
+++ tests/users.test.js
@@ -14,3 +14,9 @@ beforeAll(async () => await dbHandler.connect());
  * Clear all test data after every test.
  */
 afterEach(async () => await dbHandler.clearDatabase());
+
+
+/**
+ * Remove and close the db and server.
+ */
+afterAll(async () => await dbHandler.closeDatabase());

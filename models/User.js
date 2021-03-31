f5bcebe2847d71195493be8a76f5f9003838180a
Khoby-790 <ebaidoo79@gmail.com>
2021-03-31 12:28:31 +0000

    --- Auto-Git Commit ---

--- models/User.js
+++ models/User.js
@@ -27,7 +27,7 @@ async function verify(password, hash) {
 const schema = new Schema({
     name: { type: SchemaTypes.String, required: true },
     email: { type: SchemaTypes.String, required: true, unique: true },
-    hash: { type: SchemaTypes.String, required: false }
+    hash: { type: SchemaTypes.String, required: true }
 });
 
 

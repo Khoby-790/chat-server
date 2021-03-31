503f77abbda192c06ac3457509711137db2e6dcb
Khoby-790 <ebaidoo79@gmail.com>
2021-03-31 14:35:17 +0000

    --- Auto-Git Commit ---

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -5,11 +5,12 @@ const createUser = async ({ name, email, password }) => {
     const user = await User.findOne({ email });
     if (user) throw new Error("User already exist");
     const _ = await User.create({ name, email, hash: password });
-    // console.log(_)
     return true;
 }
 
-const 
+const authenticateUser = async ({ email, password }) => {
+    
+}
 
 module.exports = {
     createUser

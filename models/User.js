799931a669003adf8fd91c5ab2f569dc46f946d4
Khoby-790 <ebaidoo79@gmail.com>
2021-03-31 12:21:58 +0000

    --- Auto-Git Commit ---

--- models/User.js
+++ models/User.js
@@ -1,5 +1,18 @@
 const { Schema, SchemaTypes, model } = require("mongoose");
+const crypto = require("crypto")
 
+
+async function hash(password) {
+    return new Promise((resolve, reject) => {
+        // generate random 16 bytes long salt
+        const salt = crypto.randomBytes(16).toString("hex")
+
+        crypto.scrypt(password, salt, 64, (err, derivedKey) => {
+            if (err) reject(err);
+            resolve(salt + ":" + derivedKey.toString('hex'))
+        });
+    })
+}
 
 const schema = new Schema({
     name: { type: SchemaTypes.String, required: true },

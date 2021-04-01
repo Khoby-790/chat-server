a67097fe55d19a0b913de271b36c54fb8eae47d0
Khoby-790 <ebaidoo79@gmail.com>
2021-04-01 09:48:59 +0000

    --- Auto-Git Commit ---

--- tests/users.test.js
+++ tests/users.test.js
@@ -27,13 +27,15 @@ afterEach(async () => await dbHandler.clearDatabase());
  */
 afterAll(async () => await dbHandler.closeDatabase());
 
+
+
 describe('User Model', () => {
     it('should create a user and not throw an error', () => {
         expect(() => userService.createUser(user)).not.toThrow();
 
     });
 
-    it('should throw an error if user already exists', () => {
+    test('should throw and error if user is already existent', () => {
         expect(() => userService.createUser(user)).toThrow();
     });
 });
